e5bdbf383d52fc6f6a82a240035cfed862ef1792
Abhishek Sharma <121100297+ABHI-Theq@users.noreply.github.com>
2024-05-26 12:51:59 +0530

    Update AddDataToDB.py

--- AddDataToDB.py
+++ AddDataToDB.py
@@ -2,9 +2,9 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKeyACCOuntTYPE.json")
 firebase_admin.initialize_app(cred,{
-    'databaseURL':'https://faceattendancert-389ca-default-rtdb.firebaseio.com/'
+    'databaseURL':'DATABASE_URL'
 })
 
 ref=db.reference('Students')
